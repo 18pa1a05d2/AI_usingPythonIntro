a1f0de55a9fed2eea6973d0c75120c0a2ebdd821
18pa1a05d2 <18pa1a05d2@vishnu.edu.in>
2025-12-03 06:15:08 +0530

    inMemory implementation of ai using python

--- lang_demo.py
+++ lang_demo.py
@@ -3,18 +3,36 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableWithMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.prompts import MessagesPlaceholder
 load_dotenv()
 
-prompt = ChatPromptTemplate.from_template("{input}")
+prompt = ChatPromptTemplate.from_messages([MessagesPlaceholder("history"),"{input}"])
 llm = ChatOpenAI(model="gpt-4o")
 parser = StrOutputParser()
 chain = prompt | llm | parser
+
+store = {}
+session_id="maha_lakshmi_1"
+
+def get_session_history(session_id:str):
+    if session_id not in store:
+        store[session_id]= ChatMessageHistory()
+    return store[session_id]
+
+chain_with_history = RunnableWithMessageHistory(
+    chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="history",
+)
 
 while True:
     value = input("you: ")
     if value.lower() in ["exit", "quit"]:
         break
-    chain = prompt | llm | parser
+    
 #Runnable -> perform invoke only on runnable objects
-    answer = chain.invoke({"input":value})
+    answer = chain_with_history.invoke({"input":value},config={"configurable": {"session_id":session_id}})
     print("AI : "+answer)
